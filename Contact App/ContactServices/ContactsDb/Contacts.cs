47082a603b4e0a4ff40148931d507a0615d05322
Sayan-Kr-Shaw <100365014+Sayan-Kr-Shaw@users.noreply.github.com>
2022-02-24 20:31:39 +0530

    Add files via upload

---
+++ Contact App/ContactServices/ContactsDb/Contacts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactServices.ContactsDb
+{
+    public class Contacts
+    {
+        public string ContactId { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }  
+        public DateTime Marriage_Anniversary { get; set; }
+        public string? Notes { get; set; }
+    }
+}
---
+++ Contact App/ContactServices/Controllers/ContactsController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactServices.ContactsDb;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace ContactServices.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ContactsController : Controller
+    {
+       List<Contacts> Cont = new List<Contacts>();
+
+        [HttpGet]
+        public IActionResult Gets()
+        {
+            if (Cont.Count == 0)
+            {
+                return NotFound("No List Found");
+            }
+            return Ok(Cont);
+        }
+
+        [HttpGet]
+        public JsonResult GetContact(string ContactId)
+        {
+            var contact = Cont.SingleOrDefault(x=>x.ContactId == ContactId);
+            //if(contact == null)
+            //{
+            //    return NotFound("No Contact Found");
+            //}
+            return Json(contact);
+        }
+
+        [HttpPost]
+        public IActionResult Save(Contacts contact)
+        {
+            Cont.Add(contact);
+            if (Cont.Count == 0)
+            {
+                return NotFound("No List Found");
+            }
+            return Ok(Cont);
+        }
+
+        [HttpPut]
+        public IActionResult Update(ContactsDb.Contacts contacts)
+        {
+           // bool status = false;
+
+            try
+            {
+
+                Contacts prodObj = new Contacts();
+                prodObj.ContactId = contacts.ContactId;
+                prodObj.Name = contacts.Name;
+                prodObj.Birthday = contacts.Birthday;
+                prodObj.Marriage_Anniversary = contacts.Marriage_Anniversary;
+                prodObj.Notes = contacts.Notes;
+                
+            }
+
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+               // status = false;
+            }
+            return Ok(contacts);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteContact(string ContactId)
+        {
+            var contact = Cont.SingleOrDefault(x => x.ContactId == ContactId);
+            if (contact == null)
+            {
+                return NotFound("No Contact Found");
+            }
+            Cont.Remove(contact);
+            if(Cont.Count == 0)
+            {
+                return NotFound("No list found");
+            }
+            return Ok(contact);
+        }
+
+    }
+}
